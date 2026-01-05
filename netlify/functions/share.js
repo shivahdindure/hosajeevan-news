9da9ebe4fa4e36c29ebacaa571e966d6664e44fc
shivahdindure <shivahdindure@gmial.com>
2026-01-05 10:28:34 +0530

    Ignore node_modules and update Netlify function

--- netlify/functions/share.js
+++ netlify/functions/share.js
@@ -1,9 +1,13 @@
 const admin = require("firebase-admin");
 
-// Initialize Firebase Admin (Required to read your news from Firestore)
 if (!admin.apps.length) {
   admin.initializeApp({
-    projectId: "hosajeevannews",
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      // The private key needs regex to handle newline characters correctly in Netlify
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
